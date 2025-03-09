92956dd5685ba3507242b735b9f4e4891161a05c
GreeningSiren <greeningsiren@gmail.com>
2025-03-09 14:45:06 +0200

    Autofocus login and SWC

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -84,6 +84,7 @@ export default function Auth() {
               className="pl-10 w-full p-2.5 border border-gray-300 dark:border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:text-white"
               placeholder="you@example.com"
               required
+              autoFocus
             />
           </div>
         </div>
